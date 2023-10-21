3fb7b77132824b25acd34a959017518e3d0ca6cb
iyobo <phil.eki@gmail.com>
2023-10-21 12:21:32 -0500

    init

---
+++ src/generators/init/init.ts
@@ -0,0 +1,23 @@
+import { addDependenciesToPackageJson, formatFiles, Tree } from '@nx/devkit';
+import { Schema } from './schema';
+import { esbuildVersion } from '@nx/js/src/utils/versions';
+import { nxVersion } from '../../utils/versions';
+
+export async function esbuildInitGenerator(tree: Tree, schema: Schema) {
+  const task = addDependenciesToPackageJson(
+    tree,
+    {},
+    {
+      '@nx/esbuild': nxVersion,
+      esbuild: esbuildVersion,
+    }
+  );
+
+  if (!schema.skipFormat) {
+    await formatFiles(tree);
+  }
+
+  return task;
+}
+
+export default esbuildInitGenerator;
